9ab42a0c6a31f6717e0b2d88355dfb8ef187ac4d
vasco <555@qq.com>
2018-11-13 15:39:33 +0800

    rights完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -5,11 +5,56 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <el-table
+    :data="rightsList"
+    style="width: 100%">
+    <el-table-column
+      type="index"
+      >
+    </el-table-column>
+    <el-table-column
+      prop="authName"
+      label="权限名称"
+      >
+    </el-table-column>
+    <el-table-column
+      prop="path"
+      label="路径"
+      >
+    </el-table-column>
+    <el-table-column
+      prop="level"
+      label="层级">
+      <template slot-scope="scope">
+        <span v-if="scope.row.level === '0'">一级</span>
+        <span v-if="scope.row.level === '1'">二级</span>
+        <span v-if="scope.row.level === '2'">三级</span>
+      </template>
+    </el-table-column>
+  </el-table>
   </div>
+
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      rightsList: []
+    }
+  },
+  async created() {
+    let res = await this.axios.get(`rights/list`)
+    console.log(res)
+    let {
+      meta: { status },
+      data
+    } = res
+    if (status === 200) {
+      this.rightsList = data
+    }
+  }
+}
 </script>
 
 <style>
